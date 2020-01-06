9b5b660e49a442da28d015ffb99f0e8c589eb095
Merla Lucian <35219288+merlalucian@users.noreply.github.com>
2020-01-06 21:43:57 +0200

    Create vowel.java
    Vowel Replacer
    Create a function replaces all the vowels in a string with a specified character.
    
    Examples
    replaceVowels("the aardvark", '#') ➞ "th# ##rdv#rk"
    
    replaceVowels("minnie mouse", '?') ➞ "m?nn?? m??s?"
    
    replaceVowels("shakespeare", '*') ➞ "sh*k*sp**r*"
    Notes
    All characters will be in lower case.

---
+++ vowel.java
@@ -0,0 +1,7 @@
+public class Challenge {
+	public static String replaceVowels(String str, char ch) {
+		string s = Character.toString(ch);  
+		str2 = str.replaceAll("[aeiouAEIOU]", s);
+		return str2;
+	}
+}
